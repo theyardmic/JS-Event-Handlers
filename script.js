8e880593174c1a920b0f7917648667923f30b13a
Strucker.eth <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-06 18:05:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let button = document.querySelector('#btn');
 
+//Onclick event
 function handler() {
   alert("Thank you for your submission!");  //function to be called in the case of an event
   }
 
   button.addEventListener('click', handler);  //event listener
+  
+//on mouseover(hover)
+  function mouser() {
+  alert("Thank you for your submission!");
+  }
+
+  button.addEventListener('mouseover', mouser);
