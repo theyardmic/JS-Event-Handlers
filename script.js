6064439f50b4424aa77ddf5983106b981f0aaf6c
Strucker.eth <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-06 17:56:38 +0300

    Update script.js
    Various Events with listeners

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let button = document.querySelector('#btn');
 
 function handler() {
-  alert("Thank you for your submission!");
+  alert("Thank you for your submission!");  //function to be called in the case of an event
   }
 
-  button.addEventListener('click', handler);
+  button.addEventListener('click', handler);  //event listener
